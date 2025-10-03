9a5ee1396a2f6173c0ae32348cb7bcd7d890ed2d
Sabrina <sabrinacsilva09@gmail.com>
2025-10-03 18:41:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onClick = () => {
+    navLinks.classList.toggle('active');
